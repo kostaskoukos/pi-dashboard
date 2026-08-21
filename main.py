7cd5f55cc1615db17664f0f77858e0d4cb3addd4
kostaskoukos <kk.koukos@gmail.com>
2026-08-21 16:38:25 +0300

    add docker library and listing containers

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import docker
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+
+docker_client = docker.from_env()
 
 templates = Jinja2Templates(directory="templates")
 app = FastAPI()
@@ -8,6 +11,7 @@ app = FastAPI()
 
 @app.get("/", response_class=HTMLResponse)
 def read_root(request: Request):
+    containers = docker_client.containers.list(all=True)
     return templates.TemplateResponse(
-        request=request, name="index.html", context={"request": request}
+        request=request, name="index.html", context={"containers": [c.name for c in containers]}
     )
